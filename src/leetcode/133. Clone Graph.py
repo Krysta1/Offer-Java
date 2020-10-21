95e2c841313b584871e6e2d50248bb2ff818f578
xinsheng <1mlixinsheng@gmail.com>
2020-10-20 21:25:56 -0500

    10.20 update

---
+++ src/leetcode/133. Clone Graph.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return node
+
+        visited = {}
+        stack = []
+        stack.append(node)
+        visited[node] = Node(node.val)
+
+        while stack:
+            tmp = stack.pop()
+            for n in tmp.neighbors:
+                if n not in visited:
+                    stack.append(n)
+                    visited[n] = Node(n.val)
+                visited[tmp].neighbors.append(visited[n])
+
+        return visited[node]
